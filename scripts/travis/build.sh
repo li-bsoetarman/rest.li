deb1f57634b4859e541d38dde843aa67acbded3f
Benny Soetarman <bsoetarman@linkedin.com>
2020-06-22 23:16:14 -0700

    Update build.sh

--- scripts/travis/build.sh
+++ scripts/travis/build.sh
@@ -18,9 +18,9 @@ else
 fi
 
 # Run the actual build
-#./gradlew build $EXTRA_ARGS
+./gradlew build $EXTRA_ARGS
 #./gradlew --rerun-tasks :d2:test --tests com.linkedin.d2.discovery.stores.zk.ZooKeeperEphemeralStoreWithFiltersTest.testPutWithoutPrefixAndFilter
-./gradlew --rerun-tasks :d2:test
+#./gradlew --rerun-tasks :d2:test
 EXIT_CODE=$?
 
 # Kill the waiter job
